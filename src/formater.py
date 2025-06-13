e91397502101ee5cc175a849ac47f212fb679b75
ibrahimgb <ibrahim.guoual.b@gmail.com>
2025-06-13 20:18:24 +0200

    add formater

---
+++ src/formater.py
@@ -0,0 +1,59 @@
+import re
+from datetime import datetime, timezone
+from typing import Callable
+
+
+class Formater:
+    @staticmethod
+    def field_parser(*parsers: Callable[[str | None], str | None]) -> Callable[[str | None], str | None]:
+        def apply_parsers(value: str | None) -> str | None:
+            for parser in parsers:
+                value = parser(value)
+            return value
+        return apply_parsers
+
+    @staticmethod
+    def to_datetime(format: str) -> Callable[[str | None], str | None]:
+        def parse(value: str | None) -> str | None:
+            if not value:
+                return None
+            return datetime.strptime(value, format).replace(tzinfo=timezone.utc).isoformat()
+        return parse
+
+    @staticmethod
+    def to_date(format: str) -> Callable[[str | None], str | None]:
+        def parse(value: str | None) -> str | None:
+            if not value:
+                return None
+            return datetime.strptime(value, format).replace(tzinfo=timezone.utc).date().isoformat()
+        return parse
+
+    @staticmethod
+    def to_null(from_value: str) -> Callable[[str | None], str | None]:
+        def parse(value: str | None) -> str | None:
+            return None if value == from_value else value
+        return parse
+
+    @staticmethod
+    def to_int(value: str | None) -> int | None:
+        if not value:
+            return None
+        return int(float(value))
+
+    @staticmethod
+    def to_float(value: str | None) -> float | None:
+        if not value:
+            return None
+        return float(value)
+
+    @staticmethod
+    def null_to_empty_string(value: str | None) -> str:
+        return "" if value is None else value
+
+    @staticmethod
+    def left_trim(chars: str) -> Callable[[str | None], str | None]:
+        def parse(value: str | None) -> str | None:
+            if value is None:
+                return None
+            return value.lstrip(chars)
+        return parse
