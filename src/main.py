392550ad088685ad1b0b90317ea8b64329515ec5
ibrahimgb <ibrahim.guoual.b@gmail.com>
2025-06-14 22:21:08 +0200

    mot to env

--- src/main.py
+++ src/main.py
@@ -3,38 +3,34 @@ from postgresLoader import PostgresLoader
 from sqlScriptExecutor import SqlScriptExecutor
 from postgresCsvExporter import PostgresCsvExporter
 import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env
 
 def main():
     print(os.getcwd())
     processor = CSVProcessor(schema_path="../config/table_format.yml")
     res = processor.process_files("../data/raw/")
-    
-    #processed_rows, table_name = processor.process_file("../data/raw/category.csv")
     print(res)
-    loader = PostgresLoader(user='postgres', password='123', host='localhost', port='5432', database='mydatabase')
-    #loader.load_dataframe(processed_rows, table_name)
-    loader.load_all_dataframes(loader,res)
-    
-    executor = SqlScriptExecutor(
-        user='postgres',
-        password='123',
-        host='localhost',
-        port='5432',
-        database='mydatabase'
-    )
+
+    db_config = {
+        'user': os.getenv('DB_USER'),
+        'password': os.getenv('DB_PASSWORD'),
+        'host': os.getenv('DB_HOST'),
+        'port': os.getenv('DB_PORT'),
+        'database': os.getenv('DB_NAME')
+    }
+
+    loader = PostgresLoader(**db_config)
+    loader.load_all_dataframes(loader, res)
+
+    executor = SqlScriptExecutor(**db_config)
     executor.execute_sql_file('../config/map.sql')
 
-    executor = PostgresCsvExporter(
-        user='postgres',
-        password='123',
-        host='localhost',
-        port='5432',
-        database='mydatabase'
-    )
-
+    exporter = PostgresCsvExporter(**db_config)
     tables = ["category"]
-    executor.export_tables_to_csv(tables)
-    executor.close()
+    exporter.export_tables_to_csv(tables)
+    exporter.close()
 
 if __name__ == "__main__":
     main()
